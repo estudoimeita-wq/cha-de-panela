ffe4b2b906b48b6940a7284962b80bf6743dae9f
estudoimeita-wq <estudoimeita@gmail.com>
2026-01-14 10:51:07 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbwdkXrF9K-r9resj86Q4XKnLWhPuNB723L9CKoCTB83tX228GArHRi-ZOm5izYeQdJ5/exec";
+
+let escolhidos = [];
+
+fetch(API_URL)
+  .then(res => res.json())
+  .then(data => {
+    const lista = document.getElementById("lista");
+
+    data.forEach(item => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+        <img src="${item.imagem}">
+        <h3>${item.nome}</h3>
+        <p class="link">
+          <a href="${item.link}" target="_blank">
+            Caso queira uma sugestão de onde comprar o presente só entrar no link ❤️
+          </a>
+        </p>
+        ${
+          item.escolhidopor
+          ? `<p><strong>Escolhido por:</strong> ${item.escolhidopor}</p>`
+          : `<input placeholder="Seu nome" onchange="selecionar('${item.nome}', this.value)">`
+        }
+      `;
+
+      lista.appendChild(card);
+    });
+  });
+
+function selecionar(nome, pessoa) {
+  escolhidos.push({ nome, escolhidopor: pessoa });
+}
+
+function confirmar() {
+  Promise.all(escolhidos.map(item =>
+    fetch(API_URL, {
+      method: "POST",
+      body: JSON.stringify(item)
+    })
+  )).then(() => {
+    window.location.href = "obrigado.html";
+  });
+}
